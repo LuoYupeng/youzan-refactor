1dc67ee1357dbee4d1982ae232d7406c61bf0668
Luo Yupeng <461935859@qq.com>
2018-08-07 02:25:04 +0800

    增加底部导航组件

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,20 +5,49 @@ import  Vue from 'vue'
 import  axios from 'axios'
 import url from 'js/api.js'
 
+import { InfiniteScroll } from 'mint-ui';
+Vue.use(InfiniteScroll);
 
+import Foot from 'components/Foot.vue'
 
 let app = new Vue({
   el: '#app',
   data: {
-    lists: null
+    lists: null,
+    pageNum: 1,
+    pageSize: 6,
+    loading: false,
+    allLoaded: false,
   },
   created() {
-    axios.post(url.hotLists, {
-      pageNum: 1,
-      pageSize: 6
-    }).then (res => {
-      this.lists = res.data.lists
-    })
+    this.getLists()
+  },
+  methods: {
+    getLists() {
+      if(this.allLoaded) return
+      this.loading = true
+      axios.post(url.hotLists, {
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }).then (res => {
+        let curLists = res.data.lists
+        //判断所有数据加载是否完毕
+        if(curLists.length < this.pageSize) {
+          this.allLoaded = true
+        }
+        if(this.lists){
+          this.lists = this.lists.concat(curLists)
+        }else {
+          //第一次请求数据
+          this.lists = curLists
+        }
+      })
+      this.loading = false
+      this.pageNum++
+    }
+  },
+  components: {
+    Foot
   }
 })
 
